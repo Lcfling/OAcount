a17a93003591254ef5f689694f6e409f8181b575
zhangfei <752993248@qq.com>
2020-12-17 08:49:19 +0800

    上传我的代码

--- controllers/userbase.go
+++ controllers/userbase.go
@@ -20,9 +20,13 @@ type UserBaseController struct {
 func (this *UserBaseController) Prepare() {
 	token := this.Ctx.Request.Header.Get("token")
 	useridstr := this.Ctx.Request.Header.Get("userid")
-
+	if token == "" || useridstr == "" {
+		this.Data["json"] = map[string]interface{}{"code": 2, "message": "登录效验失败", "data": ""}
+		this.ServeJSON()
+	}
 	userid, _ := strconv.ParseInt(useridstr, 10, 64)
 	U, err := GetUser(userid)
+
 	if err != nil {
 		this.Data["json"] = map[string]interface{}{"code": 2, "message": "登录效验失败", "data": ""}
 		this.ServeJSON()
