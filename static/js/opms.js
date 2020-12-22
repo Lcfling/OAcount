0437b7e9462272621758b23133bd70e1315df7d9
zhangfei <752993248@qq.com>
2020-12-22 09:58:59 +0800

    上传我的任务查阅处理

--- static/js/opms.js
+++ static/js/opms.js
@@ -1946,7 +1946,53 @@ $(function(){
 			});
 		}
 	});
-	
+	//添加消息
+	$('#news-form').validate({
+		ignore:'',
+		rules : {
+		},
+		messages : {
+		},
+		submitHandler:function(form) {
+			$(form).ajaxSubmit({
+				type:'POST',
+				dataType:'json',
+				success:function(data) {
+					dialogInfo(data.message)
+					if (data.code) {
+						setTimeout(function(){window.location.href="/news/manage"}, 2000);
+					} else {
+						setTimeout(function(){ $('#dialogInfo').modal('hide'); }, 1000);
+					}
+				}
+			});
+		}
+	});
+
+	//添加消息类型
+	$('#classic-form').validate({
+		ignore:'',
+		rules : {
+		},
+		messages : {
+		},
+		submitHandler:function(form) {
+			$(form).ajaxSubmit({
+				type:'POST',
+				dataType:'json',
+				success:function(data) {
+					dialogInfo(data.message)
+					if (data.code) {
+						setTimeout(function(){window.location.href="/news/classic"}, 2000);
+					} else {
+						setTimeout(function(){ $('#dialogInfo').modal('hide'); }, 1000);
+					}
+				}
+			});
+		}
+	});
+
+
 });
 
 function dialogInfo(msg) {
